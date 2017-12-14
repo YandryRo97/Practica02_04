8277d037a6b7b3860aada9ea1cbe01e7973f595b
USUARIO <USUARIO@DESKTOP-CEEAHLH>
2017-12-14 09:49:34 -0500

    Creado la ventanaPrincipal

--- Practica02_04/src/practica02_04/Principal.java
+++ Practica02_04/src/practica02_04/Principal.java
@@ -5,6 +5,17 @@
  */
 package practica02_04;
 
+import java.util.ArrayList;
+import java.util.List;
+import practica02_04.controlador.GestionDato;
+import practica02_04.modelo.Equipo;
+import practica02_04.modelo.Estadio;
+import practica02_04.modelo.Grupo;
+import practica02_04.modelo.Jugador;
+import practica02_04.modelo.Pais;
+import practica02_04.modelo.Torneo;
+import practica02_04.vista.VentanaPrincipal;
+
 /**
  *
  * @author PC-MATIC
@@ -16,6 +27,18 @@ public class Principal {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        List<Equipo> eL = new ArrayList<Equipo>();
+        List<Estadio> esL = new ArrayList<Estadio>();
+        List<Grupo> gL = new ArrayList<Grupo>();
+        List<Jugador> jL = new ArrayList<Jugador>();
+        List<Pais> pL = new ArrayList<Pais>();
+        List<Torneo> tL = new ArrayList<Torneo>();
+       
+        
+        GestionDato gD = new GestionDato(eL, esL, gL, jL, pL, tL);
+        VentanaPrincipal v = new VentanaPrincipal("Torneo Mundial",gD);
+        v.setVisible(true);
     }
     
 }
--- Practica02_04/src/practica02_04/controlador/GestionDato.java
+++ Practica02_04/src/practica02_04/controlador/GestionDato.java
@@ -5,10 +5,85 @@
  */
 package practica02_04.controlador;
 
+import java.util.List;
+import practica02_04.modelo.Equipo;
+import practica02_04.modelo.Estadio;
+import practica02_04.modelo.Grupo;
+import practica02_04.modelo.Jugador;
+import practica02_04.modelo.Pais;
+import practica02_04.modelo.Torneo;
+
 /**
  *
  * @author Estudiante
  */
 public class GestionDato {
+    
+    private List<Equipo> equipoList;
+    private List<Estadio> estadioList;
+    private List<Grupo>  grupoList;
+    private List<Jugador> jugadorList;
+    private List<Pais> paisList;
+    private List<Torneo> torneoList;
+
+    public GestionDato(List<Equipo> equipoList, List<Estadio> estadioList, List<Grupo> grupoList, List<Jugador> jugadorList, List<Pais> paisList, List<Torneo> torneoList) {
+        this.equipoList = equipoList;
+        this.estadioList = estadioList;
+        this.grupoList = grupoList;
+        this.jugadorList = jugadorList;
+        this.paisList = paisList;
+        this.torneoList = torneoList;
+    }
+
+    public List<Equipo> getEquipoList() {
+        return equipoList;
+    }
+
+    public void setEquipoList(List<Equipo> equipoList) {
+        this.equipoList = equipoList;
+    }
+
+    public List<Estadio> getEstadioList() {
+        return estadioList;
+    }
+
+    public void setEstadioList(List<Estadio> estadioList) {
+        this.estadioList = estadioList;
+    }
+
+    public List<Grupo> getGrupoList() {
+        return grupoList;
+    }
+
+    public void setGrupoList(List<Grupo> grupoList) {
+        this.grupoList = grupoList;
+    }
+
+    public List<Jugador> getJugadorList() {
+        return jugadorList;
+    }
+
+    public void setJugadorList(List<Jugador> jugadorList) {
+        this.jugadorList = jugadorList;
+    }
+
+    public List<Pais> getPaisList() {
+        return paisList;
+    }
+
+    public void setPaisList(List<Pais> paisList) {
+        this.paisList = paisList;
+    }
+
+    public List<Torneo> getTorneoList() {
+        return torneoList;
+    }
+
+    public void setTorneoList(List<Torneo> torneoList) {
+        this.torneoList = torneoList;
+    }
+    
+    
+    
     
 }
