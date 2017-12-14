b7205e636a332faed5064ce59d9248606d2ca8e5
Estudiante <Estudiante@lab0503>
2017-12-14 10:40:37 -0500

    me falta terminar las ventanas

--- Practica02_04/src/vista/VentanaEstadio.java
+++ Practica02_04/src/vista/VentanaEstadio.java
@@ -15,6 +15,8 @@ import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.JInternalFrame;
 import controlador.GestionDato;
+import modelo.Estadio;
+import controlador.EventoEstadio;
 
 /**
  *
@@ -35,5 +37,44 @@ public class VentanaEstadio extends JInternalFrame {
     private DefaultTableModel modeloTabla;
     private JTable tabla;
     private JScrollPane scroll;
+
+    public VentanaEstadio(GestionDato gestionDato) {
+        super("Registro Estadio", true, true, true, true);
+        this.setSize(500, 300);
+        this.gestionDato = gestionDato;
+        this.iniciaComponente();
+    }
+
+    public void iniciaComponente() {
+        this.etiList = new ArrayList<JLabel>();
+        this.etiList.add(new JLabel("Ingrese el id"));
+        this.etiList.add(new JLabel("Ingrese el nombre"));
+        this.etiList.add(new JLabel("Ingrese la ciudad"));
+        this.etiList.add(new JLabel("Ingrese la capacidad"));
+
+        this.txtList = new ArrayList<JTextField>();
+        this.txtList.add(new JTextField(10));
+        this.txtList.add(new JTextField(10));
+        this.txtList.add(new JTextField(10));
+        this.txtList.add(new JTextField(10));
+        
+         this.boton = new JButton("Guardar");
+        this.boton2 = new JButton("Limpiar");
+       /** this.boton.addActionListener(new EventoEstadio(this));
+        this.boton2.addActionListener(new EventoEstadio(this));*/
+        
+        this.encabezado = new Object[4];
+        this.encabezado[0] = "Id";
+        this.encabezado[1] = "Nombre";
+        this.encabezado[2] = "Ciudad";
+        this.encabezado[3] = "Capacidad";
+        
+        Estadio e = new Estadio();
+        e.getCapacidad();
+        e.getId();
+        e.getCiudad();
+        e.getNombre();
+         
+    }
 
 }
